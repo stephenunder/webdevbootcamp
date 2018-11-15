9850a8d7b389e17322b5c7ccbbeb9da4d1e00584
Stephen <41934385+stephenunder@users.noreply.github.com>
2018-11-14 22:47:03 -0600

    YelpCamp comments pt2

---
+++ YelpCamp/v5/app.js
@@ -0,0 +1,106 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const Campground = require("./models/campground");
+const Comment = require("./models/comment");
+const seedDB = require("./seeds");
+
+
+mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true });
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+seedDB();
+
+
+app.get("/", (req, res) => {
+  res.render("landing");
+});
+
+app.get("/campgrounds", (req, res) => {
+  // Get all campgrounds from DB
+  Campground.find({}, (err, allCampgrounds) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("campgrounds/index", {campgrounds: allCampgrounds});
+    }
+  })
+});
+
+app.post("/campgrounds", (req, res) => {
+  // get data from form and add to campgrounds array
+  let name = req.body.name;
+  let image = req.body.image;
+  let desc = req.body.description;
+  let newCampground = {name: name, image: image, description: desc};
+  // Create a new campground and save to DB
+  Campground.create(newCampground, (err, newlyCreated) => {
+    if (err) {
+      console.log(err);
+    } else {
+        // redirect back to campgrounds page
+        res.redirect("/campgrounds");
+    }
+  })
+});
+
+app.get("/campgrounds/new", (req, res) => {
+  res.render("campgrounds/new");
+});
+
+app.get("/campgrounds/:id", (req, res) => {
+  // find the campground with provided ID
+  Campground.findById(req.params.id).populate("comments").exec((err, foundCampground) => {
+    if (err) {
+      console.log(err);
+    } else {
+        console.log(foundCampground);
+        // render show template with that campground
+        res.render("campgrounds/show", {campground: foundCampground});
+    }
+  });
+});
+
+// ====================
+// COMMENTS ROUTES
+// ====================
+
+app.get("/campgrounds/:id/comments/new", (req, res) => {
+  // find campground by ID
+  Campground.findById(req.params.id, (err, campground) => {
+    if(err) {
+      console.log(err);
+    } else {
+      res.render("comments/new", {campground: campground});
+    }
+  })
+});
+
+app.post("/campgrounds/:id/comments", (req, res) => {
+  // lookup campground using ID
+  Campground.findById(req.params.id, (err, campground) => {
+    if (err) {
+      console.log(err);
+      res.redirect("/campgrounds");
+    } else {
+      // create new comment
+      Comment.create(req.body.comment, (err, comment) => {
+        if (err) {
+          console.log(err);
+        } else {
+          // connect new comment to campground
+          campground.comments.push(comment);
+          campground.save();
+          // redirect to campground show page
+          res.redirect("/campgrounds/" + campground._id);
+        }
+      })
+    }
+  });
+});
+
+app.listen(3000, () => {
+  console.log("The YelpCamp Server Has Started!");
+});
---
+++ YelpCamp/v5/readme.md
@@ -0,0 +1,48 @@
+#YelpCamp
+
+* Add Landing Page
+* Add Campgrounds Page that lists all campgrounds
+
+Each Campground has:
+* Name
+* Image
+
+#Layout and Basic Styling
+* Create our header and footer partials
+* Add in Bootstrap
+
+#Creating New Campgrounds
+* Setup new campground POST route
+* Add in body-parser
+* Setup route to show form
+* Add basic unstyled form
+
+#Style the campgrounds page
+* Add a better header/title
+* Make campgrounds display in a grid
+
+#Style the Navbar and form
+* Add a navbar to all templates
+* Style the new campground form
+
+#Refactor Mongoose Code
+* Create a models directory
+* Use module.exports
+* Require everything correctly!
+
+#Add Seeds File
+* Add a seeds.js file
+* Run the seeds every time the server starts
+
+#Add the Comment model!
+* Make our errors go away!
+* Display comments on campground show page
+
+#Comment New/Create
+* Discuss nested routes
+* Add the comment new and create routes
+* Add the new comment form
+
+#Style Show Page
+* Add sidebar to show page
+* Display comments nicely
