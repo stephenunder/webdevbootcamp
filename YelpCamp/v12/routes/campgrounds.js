068b2613e0db9aaf4cd8ecaaa7edf6fe3054dfb3
Stephen <41934385+stephenunder@users.noreply.github.com>
2018-11-25 22:10:16 -0600

    landing page and dynamic pricing

--- YelpCamp/v12/routes/campgrounds.js
+++ YelpCamp/v12/routes/campgrounds.js
@@ -19,13 +19,14 @@ router.get("/", (req, res) => {
 router.post("/", middleware.isLoggedIn, (req, res) => {
   // get data from form and add to campgrounds array
   let name = req.body.name;
+  let price = req.body.price;
   let image = req.body.image;
   let desc = req.body.description;
   let author = {
     id: req.user._id,
     username: req.user.username,
   };
-  let newCampground = {name: name, image: image, description: desc, author: author};
+  let newCampground = {name: name, price: price, image: image, description: desc, author: author};
   // Create a new campground and save to DB
   Campground.create(newCampground, (err, newlyCreated) => {
     if (err) {
