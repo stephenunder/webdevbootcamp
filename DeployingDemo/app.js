239355d1fb7a91f8448cd65f32a455827f44b017
Stephen <41934385+stephenunder@users.noreply.github.com>
2018-11-27 22:53:22 -0600

    initial commit

---
+++ DeployingDemo/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.listen(3000);
