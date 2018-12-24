6c7d33c69932491e64b1d20776fc388dd64b375e
Stephen <41934385+stephenunder@users.noreply.github.com>
2018-12-23 22:39:13 -0600

    Error handling routes and middleware

--- YelpCamp/v12/routes/campgrounds.js
+++ YelpCamp/v12/routes/campgrounds.js
@@ -47,8 +47,9 @@ router.get("/new", middleware.isLoggedIn, (req, res) => {
 router.get("/:id", (req, res) => {
   // find the campground with provided ID
   Campground.findById(req.params.id).populate("comments").exec((err, foundCampground) => {
-    if (err) {
-      console.log(err);
+    if (err || !foundCampground) {
+      req.flash("error", "Campground not found");
+      res.redirect("back");
     } else {
         console.log(foundCampground);
         // render show template with that campground
